07b260c3732cce8b30888bd0530572e825ae54a2
Christopher Loental <christopherloental@christophersmbp.gateway.sonic.net>
2015-01-25 15:19:02 -0800

    Commit#2

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## Example Usage
+##
+## create a makeCacheMatrix object
+## > x <- $makeCacheMatrix
+## set matrix value using set() function (only allows square matrices)
+## > x$set(M1)
+## solve for inverse, passing in x
+## > cacheSolve(x)
 
+## try solving it again and you will see that the inverse is
+## read out from the cache
+## > cacheSolve(x)
+
+# > B
+# [,1] [,2] [,3]
+# [1,]    1   44    7
+# [2,]    2    5    8
+# [3,]    3    6    9
+# > x <- makeCacheMatrix()
+# > x$set(B)
+# > cacheSolve(x)
+# [,1]   [,2]       [,3]
+# [1,] -0.0125 -1.475  1.3208333
+# [2,]  0.0250 -0.050  0.0250000
+# [3,] -0.0125  0.525 -0.3458333
+# > cacheSolve(x)
+# getting cached data
+# [,1]   [,2]       [,3]
+# [1,] -0.0125 -1.475  1.3208333
+# [2,]  0.0250 -0.050  0.0250000
+# [3,] -0.0125  0.525 -0.3458333
+# > 
+
+### This function creates a matrix object with get & set for the matrix
+### and get & set for the matrix inverse
 makeCacheMatrix <- function(x = matrix()) {
     set <- function(y) {
       x <<- y
@@ -16,7 +47,10 @@ makeCacheMatrix <- function(x = matrix()) {
        getinv = getinv)
 }
 
-## Write a short comment describing this function
+### this function returns the inverse of matrix passed into
+### makeCacheMatrix
+### if it has been previously calculated it will return the cached value
+### if not, it will freshly calculate the value
 
 cacheSolve <- function(matx, ...) {
         ## Return a matrix that is the inverse of 'x
